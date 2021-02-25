d55a63aed0e3883673eb3e02afa8aec352444ddd
Tomas Padilla <padillatomasagustin@gmail.com>
2021-02-25 15:39:47 +0100

    ej 1 part 2

--- Section4/Excercises.java
+++ Section4/Excercises.java
@@ -1,11 +1,26 @@
 public class Excercises {
    public static void main(String[] args) {
     // Excercise 1
-        
+   toMilesPerHour(1.5);
+   
     // Excercise 2
     // Excercise 3
     // Excercise 4
     // Excercise 5
     // Excercise 6
    } 
+    //    Ex 1:
+  public static long toMilesPerHour(double kmPerHour) {
+          if(kmPerHour < 0) {
+              return -1;
+          } else  {
+              long myRound = Math.round(kmPerHour / 1.609);
+              System.out.println(myRound);
+            return myRound;   
+          } 
+       
+      }
+public static void printConversion (double kmPerHour){
+    System.out.println("hi");
+} 
 }

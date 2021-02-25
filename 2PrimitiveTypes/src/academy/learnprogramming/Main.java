42cfdd9d5b5b55afb65219e242471ad21aaaed0e
Tomas Padilla <padillatomasagustin@gmail.com>
2021-02-25 08:52:28 +0100

    Probando pasar a VSCode

--- 2PrimitiveTypes/src/academy/learnprogramming/Main.java
+++ 2PrimitiveTypes/src/academy/learnprogramming/Main.java
@@ -6,8 +6,33 @@ public class Main {
 	int myValue = 10000;
 	int myMinIntValue = Integer.MIN_VALUE;
 	int myMaxIntValue = Integer.MAX_VALUE;
-        System.out.println("Integer Minimum = " + myMinIntValue);
-        System.out.println("Integer Maximum = " + myMaxIntValue);
-        System.out.println("Overflowed the MAX Value : " + (myMaxIntValue + 1));
+	System.out.println("Integer Minimum = " + myMinIntValue);
+	System.out.println("Integer Maximum = " + myMaxIntValue);
+	System.out.println("Overflowed the MAX Value : " + (myMaxIntValue + 1));
+
+	byte myMinByte = Byte.MIN_VALUE;
+	byte myMaxByte = Byte.MAX_VALUE;
+	System.out.println(myMinByte);
+	System.out.println(myMaxByte);
+
+	short myMinShort = Short.MIN_VALUE;
+	short myMaxShort = Short.MAX_VALUE;
+	System.out.println(myMinShort);
+	System.out.println(myMaxShort);
+
+	long myLong = 100L;
+	long myMinLong = Long.MIN_VALUE;
+	long myMaxLong = Long.MAX_VALUE;
+	System.out.println(myLong);
+	System.out.println(myMinLong);
+	System.out.println(myMaxLong);
+
+	// Arithmetic and Casting
+        int myTotal = (myMinIntValue / 2);
+        byte myNewByteValue = (byte)(myMinByte / 2);
+        System.out.println(myNewByteValue);
+
+
+
     }
 }

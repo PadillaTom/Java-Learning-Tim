cdf1f8faa71befe399dd44fb38571137970ad6cc
Tomas Padilla <padillatomasagustin@gmail.com>
2021-02-25 11:53:26 +0100

    Comenzamos parte 4

--- 2PrimitiveTypes/src/academy/learnprogramming/Main.java
+++ 2PrimitiveTypes/src/academy/learnprogramming/Main.java
@@ -4,50 +4,95 @@ public class Main {
     public static void main(String[] args) {
 
 	// INT BYTE SHORT LONG ---> Primitive Types
-		int myValue = 10000;
-		int myMinIntValue = Integer.MIN_VALUE;
-		int myMaxIntValue = Integer.MAX_VALUE;
-		System.out.println(myValue);
-		System.out.println("Integer Minimum = " + myMinIntValue);
-		System.out.println("Integer Maximum = " + myMaxIntValue);
-		System.out.println("Overflowed the MAX Value : " + (myMaxIntValue + 1));
+		// int myValue = 10000;
+		// int myMinIntValue = Integer.MIN_VALUE;
+		// int myMaxIntValue = Integer.MAX_VALUE;
+		// System.out.println(myValue);
+		// System.out.println("Integer Minimum = " + myMinIntValue);
+		// System.out.println("Integer Maximum = " + myMaxIntValue);
+		// System.out.println("Overflowed the MAX Value : " + (myMaxIntValue + 1));
 
-		byte myMinByte = Byte.MIN_VALUE;
-		byte myMaxByte = Byte.MAX_VALUE;
-		System.out.println(myMinByte);
-		System.out.println(myMaxByte);
+		// byte myMinByte = Byte.MIN_VALUE;
+		// byte myMaxByte = Byte.MAX_VALUE;
+		// System.out.println(myMinByte);
+		// System.out.println(myMaxByte);
 
-		short myMinShort = Short.MIN_VALUE;
-		short myMaxShort = Short.MAX_VALUE;
-		System.out.println(myMinShort);
-		System.out.println(myMaxShort);
+		// short myMinShort = Short.MIN_VALUE;
+		// short myMaxShort = Short.MAX_VALUE;
+		// System.out.println(myMinShort);
+		// System.out.println(myMaxShort);
 
-		long myLong = 100L;
-		long myMinLong = Long.MIN_VALUE;
-		long myMaxLong = Long.MAX_VALUE;
-		System.out.println(myLong);
-		System.out.println(myMinLong);
-		System.out.println(myMaxLong);
+		// long myLong = 100L;
+		// long myMinLong = Long.MIN_VALUE;
+		// long myMaxLong = Long.MAX_VALUE;
+		// System.out.println(myLong);
+		// System.out.println(myMinLong);
+		// System.out.println(myMaxLong);
 
 	// Arithmetic and Casting
-        int myTotal = (myMinIntValue / 2);		
-        byte myNewByteValue = (byte)(myMinByte / 2);
-		System.out.println(myTotal);
-        System.out.println(myNewByteValue);
-		short myNewShortvalue = (short) (myMinShort / 2);
-		System.out.println(myNewShortvalue);
+        // int myTotal = (myMinIntValue / 2);		
+        // byte myNewByteValue = (byte)(myMinByte / 2);
+		// System.out.println(myTotal);
+        // System.out.println(myNewByteValue);
+		// short myNewShortvalue = (short) (myMinShort / 2);
+		// System.out.println(myNewShortvalue);
 
-	// CHALLENGE 1: 
+	// ------------ CHALLENGE 1 -------------
 	// Create a Byte, Short and Int.
 	// ADD 50k + 10* the sum of three vars created.
-	byte myChByte = 100;
-	short myChShort =	30000;
-	int myChInt = 69900;
-	long myChLongTotal = 50000L +10L * (myChByte + myChShort + myChInt) ;
-	System.out.println("My Challenge Total: " + myChLongTotal);
+		// byte myChByte = 100;
+		// short myChShort =	30000;
+		// int myChInt = 69900;
+		// long myChLongTotal = 50000L +10L * (myChByte + myChShort + myChInt) ;
+		// System.out.println("My Challenge Total: " + myChLongTotal);
 
 	// FLOAT DOUBLE ---> Primitive Types
-	
+		// float myMinFloatValue = Float.MIN_VALUE;
+		// float myMaxFloatValue = Float.MAX_VALUE;
+		// System.out.println(myMinFloatValue);
+		// System.out.println(myMaxFloatValue);
+		
+		// double myMinDoubleValue = Double.MIN_VALUE;
+		// double myMaxDoubleValue = Double.MAX_VALUE;
+		// System.out.println(myMinDoubleValue);
+		// System.out.println(myMaxDoubleValue);
 
+		// int myInt = 5 / 2;
+		// float myFloat = 5.25f / 2f;
+		// double myDouble = 5.25d / 2d;
+		// System.out.println("my Int: " + myInt + " Gets TRUNCATED downwards");
+		// System.out.println("my Float: " + myFloat);
+		// System.out.println("my Double: " + myDouble + " Use DOUBLE ALWAYS, they are FASTER than FLOATS");
+
+	// ------------ CHALLENGE 2 -------------
+	// Convert Punds to Kilograms.
+	// Variable to store PUNDS, Calculate, Print --> 1 Pound = 0.45359237Kg
+		// double myPounds = 100d;
+		// double myCalc = myPounds * 0.45359237d;
+		// System.out.println(myPounds + " Pounds are: " + myCalc + " Kilograms");
+
+	// CHAR and BOOLEAN ---> Primitive Types	
+		// char myChar = 'D'; //Must use SINGLE QUOTES!!!!!!
+		// char myUnicodeCHar = '\u0044';
+		// System.out.println(myChar + " Char // Unicode: " + myUnicodeCHar );
+		// char myCopyChar = '\u00A9';
+		// System.out.println(myCopyChar);
+
+		// boolean myTrue = true;
+		// boolean myFalse = false;
+		// System.out.println(myTrue + " and " + myFalse);
+
+	// STRINGS ----> Primitive Type
+		String myStr = "Hello World";
+		System.out.println(myStr);
+		myStr += " Just CONCATENATED this";
+		System.out.println(myStr);
+		myStr += ". All rights Reserved \u00A9 2021" ;
+		System.out.println(myStr);
+
+		int myIntStr = 50;
+		String myStrforInt = "10";
+		System.out.println("Adding an INT to a STR: " + myIntStr + myStrforInt + "Java knows im Adding INT to STR therefore converts the INT to STR");
     }
+
 }
---
+++ 3OperatorsExpressions/OperatorsExpressions.java
@@ -0,0 +1,76 @@
+import jdk.javadoc.internal.doclets.formats.html.SystemPropertiesWriter;
+
+public class OperatorsExpressions {
+    public static void main(String[] args) {
+       
+    // Operators and Expressions:
+        // int result = 1 + 2 ;
+        // System.out.println("1 + 2 = "  + result);
+        // int prevResult = result;
+        // System.out.println("Prev Result = " + prevResult);
+        // result -= 1;
+        // System.out.println("3 - 1 = " + result);
+        // System.out.println("Prev Res ... After changing normal Result = " + prevResult);
+
+        // result *= 10;
+        // System.out.println("2 * 10" +result);
+        // result /= 5;
+        // System.out.println("20 / 5"+result);
+        // result %= 3;
+        // System.out.println("Modulus(Remainder after division) of 4 = " + result);
+    
+    // IF - THEN Statements: Conditional Logic
+        // boolean isAlien = false;
+        // if(isAlien == false) {
+        //     System.out.println("It is NOT an Alien");
+        //     System.out.println("Second line inside CODE BLOCK");
+        // } else {
+        //     System.out.println("It IS an Alien");
+        // }
+
+    // Logical Operators: >, =, <, >=, <=, == , AND(&&, both true), OR(||, only one true),
+        // int topScore = 80;
+        // if (topScore < 100){
+        //     System.out.println("You got HIGHEST Score");
+        // }   
+        // int secondTopScore = 60;
+        // // if ((topScore > secondTopScore) && (topScore < 100)){
+        // // System.out.println("Greater than second , but less than 100");
+        // // }   
+        // if ((topScore > 90) || (secondTopScore <= 90)){
+        // System.out.println("either of both conditions are true");
+        // }
+
+    // One = (Assignment Operator) is to assign, Double == is to test
+    // One & or | is to comparte bitwise rather than values.
+
+    // Not Operator !:
+    //     boolean isCar = false;
+    //     if (!isCar) {
+    //         System.out.println("Car: False");
+    //     }
+    // Ternary Operator: (___) ? ___ : ___ ;
+    //     isCar = true;
+    //     boolean wasCar = (isCar) ? true : false;
+    //     if(wasCar){
+    //     System.out.println(wasCar);
+    //     }
+
+    // ---------- CHALLENGE 3 ------------
+    // Create two Doubles: 20.00 and 80.00. Sum them and * 100.00.
+    // % of the result and 40.00
+    // Boolean true if is 0, false not 0. Then Print it
+    // Else Print "Got some Reminder" if not 0.
+
+    double myFirst = 20.00d;
+    double mySec = 80.00d;
+    double firstRes = (myFirst + mySec) * 100.00d;
+    double remainderOfRes = firstRes % 40.00d;
+    boolean isBool = ( remainderOfRes == 0) ? true : false;
+    if (isBool) {        
+        System.out.println("My Boolean is: " + isBool);
+    } else {       
+        System.out.println("Got some Remainder");
+    }
+    }
+}

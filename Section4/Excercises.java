e472ca32197255ff2356e3a607082b74439cea3b
Tomas Padilla <padillatomasagustin@gmail.com>
2021-02-27 08:35:39 +0100

    Ex 3

--- Section4/Excercises.java
+++ Section4/Excercises.java
@@ -1,26 +1,44 @@
 public class Excercises {
    public static void main(String[] args) {
     // Excercise 1
-   toMilesPerHour(1.5);
-   
+        // toMilesPerHour(1.5);
+        // printConversion(2.00);   
     // Excercise 2
+        printMbAndKb(2500);
+
     // Excercise 3
     // Excercise 4
     // Excercise 5
     // Excercise 6
    } 
-    //    Ex 1:
-  public static long toMilesPerHour(double kmPerHour) {
-          if(kmPerHour < 0) {
-              return -1;
-          } else  {
-              long myRound = Math.round(kmPerHour / 1.609);
-              System.out.println(myRound);
-            return myRound;   
-          } 
-       
+    //  Ex 1:
+    //   public static long toMilesPerHour(double kmPerHour) {
+    //           if(kmPerHour < 0) {
+    //               return -1;
+    //           } else  {
+    //               long myRound = Math.round(kmPerHour / 1.609);
+    //               System.out.println(myRound);
+    //             return myRound;   
+    //           } 
+          
+    //       }
+    // public static void printConversion (double kmPerHour){
+    //   long myMiles = Math.round(kmPerHour * 0.621371)  ;
+    //   if(kmPerHour < 0 ) {
+    //     System.out.println("Invalid Value");
+    //   } else {
+    //     System.out.println(kmPerHour + " Km/h" +" = "+ (double) myMiles + " Mi/h");
+    //   }
+      
+    // } 
+    //  Ex 2:
+      public static void printMbAndKb(int Kb) {
+        if(Kb < 0 ){
+          System.out.println("Invalid Value");
+        } else {
+          int myMb = Kb / 1024;
+          int myRem = Kb % 1024;
+          System.out.println(Kb + " KB = " + myMb + " MB and " + myRem + " KB" );
+        }
       }
-public static void printConversion (double kmPerHour){
-    System.out.println("hi");
-} 
 }

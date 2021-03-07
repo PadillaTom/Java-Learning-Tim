382bc55735e14e13b9416ca54f25f150c0d4185a
Tomas Padilla <padillatomasagustin@gmail.com>
2021-03-07 07:43:11 +0100

    OOPS

--- Section6OOP/src/com/classes/Car.java
+++ Section6OOP/src/com/classes/Car.java
@@ -4,12 +4,28 @@ public class Car {
 //    These variables are STATE of the car.
     private int doors;
     private int wheels;
-    public String model;
+    private String model;
     private String engine;
     private String colour;
 //    PUBLIC = We can access from other files
 //    PRIVATE = Only can access within this class.
-//    Methods:
+
+//    Methods
+//    --> SETTER:
+    public void setModel(String model){
+//        Example of Validation: Helpful to prevent mistakes, Making the data
+//        comming IN and OUT of a CLASS are correct.
+        String validModel = model.toLowerCase();
+        if (validModel.equals("carrera") || validModel.equals("commodore")) {
+        this.model = model;
+        } else {
+            this.model = "Unknown";
+        }
+    }
+//   --> GETTER:
+    public String getModel() {
+        return this.model;
+    }
 
 
 }
--- Section6OOP/src/com/classes/OOPGeneral.java
+++ Section6OOP/src/com/classes/OOPGeneral.java
@@ -2,11 +2,11 @@ package com.classes;
 
 public class OOPGeneral {
     public static void main (String[] args){
-//  Creating and object from our CAR CLASS:
+//  Creating and object from our CAR CLASS (Initialize):
         Car porsche = new Car();
         Car holden = new Car();
-        porsche.model   = "Carrera";
-        System.out.println(porsche.model);
-
+//  Accessing the method inside the class.
+        porsche.setModel("Carrera");
+        System.out.println("Porsche Car, Model: " + porsche.getModel());
     }
 }

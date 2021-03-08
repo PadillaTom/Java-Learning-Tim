3543c9ea0cbc96c70c2762ab2fa4f3be4e2da323
Tomas Padilla <padillatomasagustin@gmail.com>
2021-03-08 17:44:28 +0100

    Composition Exercises

---
+++ Section6OOP2/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main (String[] args) {
+//        ********* COMPOSITION **********
+//        We create CLASSES that hold Methods, Variables, Classes...
+//        Then we can Create a PARENT Class, that holds The stuff said before
+
+        Dimensions dimensions = new Dimensions(20,20,5);
+
+        Case theCase = new Case("220B", "Dell", "240", dimensions );
+        Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27, new Resolution(2540, 1440));
+        Motherboard theMotherboard = new Motherboard("BJ-200", "Asus", 4, 6, "v2.44");
+
+        PC thePC = new PC (theCase, theMonitor, theMotherboard);
+//  Accessing Methods inside PC->Classes
+        thePC.getMonitor().drawPixelAt(1500,1200, "red");
+        thePC.getMotherboard().loadProgram("Windows 1.0");
+        thePC.getTheCase().pressPowerButton();
+    }
+
+}
---
+++ Section6OOP2/src/Resolution.java
@@ -0,0 +1,17 @@
+public class Resolution {
+    private int width;
+    private int height;
+
+//    Constructors:
+    public Resolution(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+//    Getters:
+    public int getWidth() {
+        return width;
+    }
+    public int getHeight() {
+        return height;
+    }
+}

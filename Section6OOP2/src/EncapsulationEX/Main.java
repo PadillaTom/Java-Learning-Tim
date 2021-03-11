87ab2eddde1fcc8b69e20c67abab217dad917085
Tomas Padilla <padillatomasagustin@gmail.com>
2021-03-11 07:18:07 +0100

    Polyphormism

---
+++ Section6OOP2/src/EncapsulationEX/Main.java
@@ -0,0 +1,15 @@
+package EncapsulationEX;
+
+public class Main {
+    public static void main (String[] args) {
+        Printer printer = new Printer (50, true);
+        System.out.println("Initial Page count = " + printer.getPagesPrinted());
+        int pagesPrinted = printer.printPages(4);
+        System.out.println("Pages printed was " + pagesPrinted + ", new count of pages printed: " + printer.getPagesPrinted());
+        pagesPrinted = printer.printPages(2);
+        System.out.println("Pages printed was " + pagesPrinted + ", new count of pages printed: " + printer.getPagesPrinted());
+
+
+
+    }
+}

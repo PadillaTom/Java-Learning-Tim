240021e7ab9f03f44b59f446cc8f469a244c9c52
Tomas Padilla <padillatomasagustin@gmail.com>
2021-03-11 08:56:52 +0100

    Arrays Comenzamos

---
+++ Section8/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+//  Store multiple values on the same time:
+        int[] myIntArray= new int[10];
+        myIntArray[5] = 50;
+
+    }
+}

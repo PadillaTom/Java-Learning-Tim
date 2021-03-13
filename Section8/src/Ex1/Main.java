ab085d853f57132dedd843268ea45e5ead99ce9b
Tomas Padilla <padillatomasagustin@gmail.com>
2021-03-13 13:04:59 +0100

    ARray CHallenge

---
+++ Section8/src/Ex1/Main.java
@@ -0,0 +1,42 @@
+package Ex1;
+
+import java.util.Scanner;
+
+public class Main {
+//    Allowing user Input:
+    public static Scanner scanner = new Scanner(System.in);
+
+//    Main:
+    public static void main(String[] args) {
+
+        int[] myInts = getIntegers(5);
+
+//        Printing out the Numbers:
+        for(int i=0; i<myInts.length; i++){
+            System.out.println("Element: " + i +" , value:" + myInts[i]);
+        }
+//        Average:
+        System.out.println("Average is: " + getAverage(myInts));
+    }
+
+//Getting integers from Scanner and adding them to Array:
+    public static int[] getIntegers(int number) {
+        System.out.println("Enter " + number + " integer values.\r");
+
+        int[] values = new int[number];
+        for(int i=0; i< values.length; i++){
+            values[i] = scanner.nextInt();
+        }
+        return values;
+    }
+
+//    METHOD: Get the Average of all values entered.
+    public static double getAverage(int[] array){
+        int sum = 0;
+        for(int i=0; i< array.length; i++){
+            sum += array[i];
+        }
+        return (double)sum / (double)array.length;
+    }
+
+}
--- Section8/src/Main.java
+++ Section8/src/Main.java
@@ -1,8 +1,41 @@
+
+
 public class Main {
     public static void main(String[] args) {
-//  Store multiple values on the same time:
-        int[] myIntArray= new int[10];
-        myIntArray[5] = 50;
+//  *** Arrays *** Store multiple values on the same time.
 
+//  Creating an Array: Specifying type and amount of values.
+        int[] myIntArray = new int[10]; // Number of elements, Starts with 0.
+//        myIntArray[5] = 50;
+//        System.out.println(myIntArray[5]);
+//        double[] myDoubleArray = new double[10];
+//        myDoubleArray[5] = 12.5;
+//        System.out.println(myDoubleArray[5]);
+
+//  Creating Array with the value already set:
+//        int[] myValuesArray = {1,2,3,4,5,6};
+//        System.out.println(myValuesArray[1]);
+//        System.out.println(myValuesArray[5]);
+
+//  Creating an Array with a FOR LOOP:
+//    for (int i=0; i<10; i++) {
+//        myIntArray[i] = i;
+//        System.out.println("Index: " + i + " || Value: " + myIntArray[i]);
+//    }
+//    for (int i=0; i < myIntArray.length; i++){
+//        myIntArray[i] = i*10;
+//        System.out.println("Index: " + i + " || Value: " + myIntArray[i]);
+//        }
+
+//  Call PRINTARRAY method:
+        printArray(myIntArray);
     }
+
+//  Method to print any array:
+    public static void printArray(int[] array) {
+        for(int i=0; i <array.length; i++){
+            System.out.println("Index: " + i + " || Value: " + array[i]);
+        }
+    }
+
 }

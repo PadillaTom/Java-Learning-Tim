5b0d3d0283a7ed8e1cfd444b73a4d3c43e276e55
Tomas Padilla <padillatomasagustin@gmail.com>
2021-03-19 11:53:27 +0100

    Abstract Classes

--- Section9/src/InnerClasses/Main.java
+++ Section9/src/InnerClasses/Main.java
@@ -2,6 +2,25 @@ package InnerClasses;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main");
+        Gearbox mcLaren = new Gearbox(6);
+
+//        Using PUBLIC Inner Class:
+//        Gearbox.Gear first = mcLaren.new Gear(1,12.3);
+//        System.out.println(first.driveSpeed(1000));
+
+//        Using PRIVATE inner class and some more code:
+//        Evitamos acceder a GEAR inner class, encapsulate it.
+
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(1);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(1000));
+        mcLaren.changeGear(2);
+        System.out.println(mcLaren.wheelSpeed(3000));
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(3);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(6000));
+
     }
 }
